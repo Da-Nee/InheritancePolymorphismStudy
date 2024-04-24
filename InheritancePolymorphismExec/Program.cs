dd610cd730b196bd90c93525a981d3918256bb0d
Felipe Yohan Sapio <yohansapio@rocketmail.com>
2024-04-24 00:51:37 -0300

    Commit

---
+++ InheritancePolymorphismExec/Program.cs
@@ -0,0 +1,57 @@
+﻿using InheritancePolymorphismExec.Entities;
+using System.Globalization;
+
+namespace InheritancePolymorphismExec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char ch = char.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (ch == 'c')
+                {
+                    products.Add(new Product(name, price));
+                }
+                else if (ch == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+
+                    products.Add(new UsedProduct(name, price, date));
+                }
+                else
+                {
+                    Console.Write("Customs fee: ");
+                    double fee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    products.Add(new ImportedProduct(name, price, fee));
+                }
+
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.PriceTag());
+            }
+        }
+    }
+}
